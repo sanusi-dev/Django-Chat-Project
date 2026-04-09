fd9ed50793507d587f37b9b41651ba9744d317fd
sanusi-dev <thebeliever002@gmail.com>
2026-04-09 05:06:04 +0100

    Update superuser creation logic to handle password updates for existing users

--- build.sh
+++ build.sh
@@ -6,15 +6,19 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
+
 if [ "$RENDER" = "true" ]; then
     python manage.py shell -c "
 from django.contrib.auth import get_user_model
-import os
+from decouple import config
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', config('DJANGO_SUPERUSER_PASSWORD', default='changeme'))
     print('Superuser created.')
 else:
-    print('Superuser already exists, skipping.')
+    u = User.objects.get(username='admin')
+    u.set_password(config('DJANGO_SUPERUSER_PASSWORD', default='changeme'))
+    u.save()
+    print('Superuser password updated.')
 "
 fi
